58cd9c656d71b00ed1d5511eef5e8b75d427c61d
jladrover <ja4146@nyu.edu>
2023-08-18 20:00:54 -0400

    redploying for vercel

--- api/index.js
+++ api/index.js
@@ -7,18 +7,18 @@ const { default: mongoose } = require('mongoose');
 
 app.use(cors());
 app.use(express.json());
-app.get('/apis/test', (request, response) => {
+app.get('/api/test', (request, response) => {
     response.json('test-working!')
 });
 
-app.post('/apis/transaction', async (request,response) => {
+app.post('/api/transaction', async (request,response) => {
     await mongoose.connect(process.env.MONGODB_URL);
     const {name,description, datetime, price} = request.body;
     const transaction = await Transaction.create({name,description,datetime, price});
     response.json(transaction);
 });
 
-app.get('/apis/transactions', async (request, response) => {
+app.get('/api/transactions', async (request, response) => {
     await mongoose.connect(process.env.MONGODB_URL);
     const totalTransactions = await Transaction.find();
     response.json(totalTransactions);
